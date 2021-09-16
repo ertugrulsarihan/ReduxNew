e05a16c81fd7291e9894c27d07db37acca23752c
ertugrulsarihan <ertugrulsarihan1@gmail.com>
2021-09-16 12:42:39 +0300

    second commit

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-const [courses, setCourses] = useState({
-  title: "",
-});
-function CoursesPage() {
+import * as actionsCourse from "../../redux/actions/actionsCourse";
+import propTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
+
+function CoursesPage(props) {
+  const [courses, setCourses] = useState(null);
   const handleChange = (event) => {
-    const course = { ...courses, title: event.target.value };
-    setCourses(course);
+    const newCourse = { ...courses, title: event.target.value, id: uuidv4() };
+    setCourses(newCourse);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(courses.title);
+    console.log(props.courses);
+    props.dispatch(actionsCourse.createCourse(courses));
   };
+
+  useEffect(() => {
+    console.log(props.courses);
+  });
 
   return (
     <div>
@@ -21,9 +28,25 @@ function CoursesPage() {
         <h3>Add Course</h3>
         <input type="text" onChange={(e) => handleChange(e)} />
         <input type="submit" />
+        {props.courses.map((m) => (
+          <div key={m.id}>
+            <h5>{m.title}</h5>
+          </div>
+        ))}
       </form>
     </div>
   );
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  dispatch: propTypes.func.isRequired,
+  courses: propTypes.array.isRequired,
+};
+
+function mapStateTopProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+export default connect(mapStateTopProps)(CoursesPage);
